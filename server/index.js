e782e6031dc9ec9e5c1ef41143f66977bd1046d8
Parikhit <parikhitritzz@gmail.com>
2024-03-16 00:30:57 +0530

    Created custom middleware fn to handle possible errorrs

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+//Middleware to handle possible errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({ success: false, statusCode, message });
+});
+
 mongoose
     .connect(process.env.MONGO)
     .then(() => console.log('Connected to MongoDB'))
