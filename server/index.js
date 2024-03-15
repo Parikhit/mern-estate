5cb1a8cd6e42c66c5220f1b592a11f41b27c3054
Parikhit <parikhitritzz@gmail.com>
2024-03-15 11:51:17 +0530

    Adding user model

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log(error));
 
 const app = express();
 
