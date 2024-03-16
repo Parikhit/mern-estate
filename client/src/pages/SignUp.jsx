e7bcf0a3c0baf868466b4dc93d172ed4674c190c
Parikhit <parikhitritzz@gmail.com>
2024-03-16 23:09:18 +0530

    Completed sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,7 +43,7 @@ const SignUp = () => {
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
-            <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+            <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
 
             <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
                 <input
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -23,14 +23,10 @@ export async function signin(req, res, next) {
 
     try {
         const validUser = await User.findOne({ email });
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
 
-        if (!validUser) {
-            return next(errorHandler(404, 'User not found!'));
-        }
-        if (!validPassword) {
-            return next(errorHandler(401, 'Wrong credentials!'));
-        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
 
         //JWT to store cookie
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
