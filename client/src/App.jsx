cf70228504270f58e84d5ebaae410800e852fbb9
Parikhit <parikhitritzz@gmail.com>
2024-04-18 14:55:47 +0530

    Complete update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
 
 //components
 import Header from './components/Header.component';
@@ -42,6 +43,10 @@ const App = () => {
                     <Route
                         path='/create-listing'
                         element={<CreateListing />}
+                    />
+                    <Route
+                        path='/update-listing/:listingId'
+                        element={<UpdateListing />}
                     />
                 </Route>
             </Routes>
--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -45,3 +45,15 @@ export async function updateListing(req, res, next) {
         next(error);
     }
 }
+
+export async function getListing(req, res, next) {
+    try {
+        const listing = await Listing.findById(req.params.id);
+
+        if (!listing) return next(errorHandler(404, 'Listing not found!'));
+
+        res.status(200).json(listing);
+    } catch (error) {
+        next(error);
+    }
+}
