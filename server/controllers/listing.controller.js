0a641adecfe633c52571c29e91e052cef7bfd53a
Parikhit <parikhitritzz@gmail.com>
2024-04-18 13:02:52 +0530

    Created update listing api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -26,3 +26,22 @@ export async function deleteListing(req, res, next) {
         next(error);
     }
 }
+
+export async function updateListing(req, res, next) {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) return next(errorHandler(404, 'Listing not found!'));
+
+    if (req.user.id !== listing.userRef)
+        return next(errorHandler(401, 'You can only delete your own listing!'));
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+}
