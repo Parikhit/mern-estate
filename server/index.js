b2b0abccf4797f3b06c137118f2ed7ed9167692b
Parikhit <parikhitritzz@gmail.com>
2024-04-21 11:08:56 +0530

    Add pre deployment setup

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -17,6 +19,12 @@ app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client', 'dist', 'index.html'));
+});
 
 //Middleware to handle possible errors
 app.use((err, req, res, next) => {
