946a4d7cb92d38bef299e3e336c3515c603c4a9e
Parikhit <parikhitritzz@gmail.com>
2024-04-21 12:04:38 +0530

    build project

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ mongoose
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.listen(3000, () => {
